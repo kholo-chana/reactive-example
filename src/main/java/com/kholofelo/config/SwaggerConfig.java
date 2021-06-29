27c3284636f02b67d8993662103fe264a13c0033
kholofelo <kholofelo.Ramile@idolconsulting.co.za>
2021-06-29 13:42:22 +0200

    added swagger documentation

--- src/main/java/com/kholofelo/config/SwaggerConfig.java
+++ src/main/java/com/kholofelo/config/SwaggerConfig.java
@@ -2,14 +2,15 @@ package com.kholofelo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
+//@Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
@@ -35,4 +36,7 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
                 .paths(PathSelectors.any())
                 .build();
     }
+
 }
+
+
